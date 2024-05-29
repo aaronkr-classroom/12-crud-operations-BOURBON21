713b1962c271d6bd0790a657519158973f3ed029
BOURBON21 <163214851+BOURBON21@users.noreply.github.com>
2024-05-29 01:08:22 +0000

    end

--- lesson-19/main.js
+++ lesson-19/main.js
@@ -15,8 +15,13 @@ const express = require("express"), // express를 요청
  */
 const mongoose = require("mongoose"); // mongoose를 요청
 // 데이터베이스 연결 설정
-mongoose.connect("mongodb://127.0.0.1:27017/ut-nodejs", {
-  useNewUrlParser: true,
+mongoose.connect(  
+  "mongodb+srv://ut-node:cJVs2c1wdnhfupa8@ut-node.br8wxbu.mongodb.net/?retryWrites=true&w=majority&appName=UT-Node",
+);
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("connected to DB!!!");
 });
 
 app.set("port", process.env.PORT || 3000);
